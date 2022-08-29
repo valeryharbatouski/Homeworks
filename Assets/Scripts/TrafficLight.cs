a92a4cb38178ffe9fa7e200c2dba1e07bf6e7082
valeryharbatouski <112054601+valeryharbatouski@users.noreply.github.com>
2022-08-30 02:25:14 +0300

    copy project to repo

---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("I'm starting as Light");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("asdasd");
+    }
+}
